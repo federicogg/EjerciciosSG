117a6ce237f6b4b4153a488f9098ba26cd00fcc9
Fede <fedeinformatica9@gmail.com>
2020-04-20 07:43:17 +0200

    commit

---
+++ 6/Model.js
@@ -0,0 +1,20 @@
+class Model extends THREE.Object3D
+{
+    constructor()
+    {
+        super();
+
+        var that = this ;
+        var materialLoader = new THREE. MTLLoader ();
+        var objectLoader = new THREE. OBJLoader ();
+        materialLoader.load('porsche911/911.mtl',
+        function ( materials ) {
+            objectLoader.setMaterials (materials);
+            objectLoader.load( 'porsche911/Porsche_911_GT2.obj',
+            function ( object ) {
+                var modelo = object;
+                that.add(modelo) ;
+            } , null , null ) ;
+        });
+    }
+}
